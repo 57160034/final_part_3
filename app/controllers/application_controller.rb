99e1a44c4c4e7e57d59a67fae646e5505e638d1d
57160034 <57160034@go.buu.ac.th>
2016-12-03 15:12:26 +0700

    Add home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+   def home
+ render html: "เพลงพระราชนิพนธ์"
+ end
+
+    def kingsong1
+ render html: "เพลง 1"
+ end 
+
+    def kingsong2
+ render html: "เพลง 2"
+ end 
+
 end
